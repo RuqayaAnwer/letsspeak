d96a6444d40fb1b8e2080e76396099d21a010e2d
ruqayaanwar <ruqayaanwer5@gmail.com>
2026-04-15 18:53:59 +0300

    chore: Remove temporary diagnostic and testing files

--- backend/test_columns.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$columns = \Illuminate\Support\Facades\Schema::getColumnListing('courses');
-print_r($columns);
